2d8876ebab32eae08d75ccc93cc065cffdbb768a
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-03-13 21:38:50 -0500

    Update theme_fng.php

--- lang/es/theme_fng.php
+++ lang/es/theme_fng.php
@@ -1,20 +1,20 @@
 <?php
-$string['pluginname'] = 'fng';
-$string['choosereadme'] = 'IngeWeb es un tema creado por el Ing. Pablo A Pico (<a href="http://ingeweb.co/">IngeWeb</a>) exclusivamente para nuestros clientes.';
+$string['pluginname'] = 'FNG';
+$string['choosereadme'] = 'FNG es un tema creado por IngeWeb (<a href="http://ingeweb.co/">IngeWeb</a>) exclusivamente para el Fondo Nacional de Garantías.';
 
-$string['configtitle'] = 'fng';
+$string['configtitle'] = 'Tema FNG';
 
 $string['logo'] = 'Logo';
 
-$string['themesettings'] = 'Configuración del Tema fng';
+$string['themesettings'] = 'Configuración del Tema FNG';
 $string['themesettingsgeneral'] = 'Configuración General';
 $string['contactinfo'] = 'Información de contacto';
 $string['abouttitle'] = 'Título Acerca de';
 $string['abouttitledesc'] = 'Título para la sección "Acerca de" en el pie de página';
-$string['abouttitle_default'] = 'Acerca de Nosotros';
+$string['abouttitle_default'] = 'Acerca del Fondo Nacional de Garantías';
 
 $string['abouttext'] = 'Texto Acerca de';
-$string['abouttextdesc'] = 'Contenido para la sección "Acerca de" en el pie de página';
+$string['abouttextdesc'] = 'Contenido para la sección "Acerca de" en el pie de página. Use esta área para proporcionar información sobre el Fondo Nacional de Garantías.';
 $string['focusmode']='Modo de enfoque';
 $string['loginfootertext'] = 'Pie de página del login';
 $string['loginfootertextdesc'] = 'Contenido para el área del pie de página en la página de inicio de sesión';
@@ -40,8 +40,8 @@ $string['copyrightdesc'] = 'Acerca de este sitio web.';
 $string['customcss'] = 'CSS Personalizado';
 $string['customcssdesc'] = 'Cualquier regla CSS que agregue a esta área se reflejará en cada página, facilitando la personalización de este tema.';
 
-$string['themeinfotext'] = 'Este tema fue creado para <strong>fng</strong> por <a target="_blank" href="http://ingeweb.co/">IngeWeb - Soluciones para triunfar en Internet</a>.';
-$string['credit'] = 'Copyright © 2025 - Todos los derechos reservados';
+$string['themeinfotext'] = 'Este tema fue creado para el <strong>Fondo Nacional de Garantías</strong> por <a target="_blank" href="http://ingeweb.co/">IngeWeb - Soluciones para triunfar en Internet</a>.';
+$string['credit'] = 'Copyright © 2025 Fondo Nacional de Garantías - Todos los derechos reservados';
 
 $string['showingonlycategorieswithenrolledcourses'] = 'Mostrando solo categorías con cursos matriculados';
 $string['nocoursestoshow'] = 'No se encontraron cursos para mostrar';
@@ -61,26 +61,41 @@ $string['hide'] = 'Ocultar';
 
 // Configuraciones del carrusel
 $string['carouselsettings'] = 'Configuración del Carrusel';
-$string['carouselsettings_desc'] = 'Configure los ajustes para el carrusel de la página de inicio de sesión incluyendo imágenes, textos y otras opciones.';
+$string['carouselsettings_desc'] = 'Configure el carrusel en la página de inicio de sesión, incluyendo imágenes, títulos y enlaces. Esto crea una experiencia visual atractiva para los usuarios.';
 
 $string['enablecarousel'] = 'Habilitar carrusel de la página de inicio';
 $string['enablecarousel_desc'] = 'Habilite esta configuración para mostrar un carrusel de imágenes en la página de inicio de sesión.';
 $string['numberofslides'] = 'Número de diapositivas';
-$string['numberofslides_desc'] = 'Seleccione el número de diapositivas para mostrar en el carrusel.';
-$string['slideimage'] = 'Imagen de diapositiva {$a}';
-$string['slideimage_desc'] = 'Subir una imagen para la diapositiva {$a} en el carrusel.';
-$string['carouselinterval'] = 'Intervalo del Carrusel';
-$string['carouselintervaldesc'] = 'Ingrese el tiempo de intervalo en milisegundos para las diapositivas del carrusel.';
+$string['numberofslides_desc'] = 'Seleccione cuántas diapositivas desea mostrar en el carrusel de la página de inicio de sesión. Cada diapositiva puede configurarse individualmente.';
 
+$string['slideheading'] = 'Configuración de Diapositiva {$a}';
+$string['slideheading_desc'] = 'Configure todos los ajustes para la diapositiva {$a} del carrusel de inicio de sesión.';
+
+$string['slideshowtitle'] = 'Mostrar título para diapositiva {$a}';
+$string['slideshowtitle_desc'] = 'Marque esta opción si desea mostrar un título sobre esta diapositiva.';
+
+$string['slideenableurl'] = 'Habilitar enlace para diapositiva {$a}';
+$string['slideenableurl_desc'] = 'Marque esta opción si desea que esta diapositiva sea clickeable y enlace a una URL.';
+
+$string['slidetitle'] = 'Título para la diapositiva {$a}';
+$string['slidetitledesc'] = 'Ingrese el texto del título que aparecerá sobre esta diapositiva. Solo visible si "Mostrar título" está habilitado.';
+
+$string['slideurl'] = 'URL del botón de la diapositiva {$a}';
+$string['slideurldesc'] = 'Ingrese la URL a la que enlazará esta diapositiva cuando se haga clic. Solo activo si "Habilitar enlace" está habilitado.';
+
+$string['slideimage'] = 'Imagen de diapositiva {$a}';
+$string['slideimage_desc'] = 'Suba una imagen para la diapositiva {$a} en el carrusel. Tamaño recomendado: 1920×1080px o mayor con la misma relación de aspecto.';
+
+$string['carouselinterval'] = 'Intervalo del Carrusel';
+$string['carouselintervaldesc'] = 'Establezca el intervalo de tiempo en milisegundos entre las transiciones de diapositivas. Un valor de 5000 equivale a 5 segundos entre diapositivas.';
+
+$string['default_slide_title'] = 'Bienvenido al Fondo Nacional de Garantías';
+
+// Cadenas para el carrusel
 $string['carousel'] = 'Carrusel de imágenes';
 $string['loginsection'] = 'Sección de inicio de sesión';
 $string['previous'] = 'Ir a la diapositiva anterior';
 $string['next'] = 'Ir a la siguiente diapositiva';
-
-$string['slidetitle'] = 'Título para la diapositiva {$a}';
-$string['slidetitledesc'] = 'Ingrese el título para la diapositiva {$a}';
-$string['slideurl'] = 'URL del botón de la diapositiva {$a}';
-$string['slideurldesc'] = 'Ingrese la URL para el botón en la diapositiva {$a}. Esta URL se abrirá en una nueva ventana.';
 
 // Cadenas de autorización
 $string['unauthorized_access'] = 'Acceso No Autorizado';
@@ -104,33 +119,33 @@ $string['mycoursesheaderdesc'] = 'Suba una imagen que se mostrará como encabeza
 // Definición de caché
 $string['cachedef_fontawesomeremuiiconmapping'] = 'Almacenamiento en caché para Font Awesome';
 
-// Nuevas cadenas necesarias para el settings.php actualizado
+// Nuevas cadenas para el settings.php actualizado
 $string['generalsettings'] = 'Configuración General';
 $string['generalnotice'] = 'Aviso General';
-$string['generalnoticedesc'] = 'Esto es útil para alertar a los usuarios de una situación particular. La alerta aparece en la portada.';
-$string['generalnotice_create'] = 'Configurar un aviso';
+$string['generalnoticedesc'] = 'Ingrese un aviso para todo el sitio que se mostrará en la parte superior. Útil para anuncios importantes, información de mantenimiento u otros mensajes críticos.';
+$string['generalnotice_create'] = 'Crear un aviso';
 $string['generalnoticeheading'] = 'Configuración de Avisos Generales';
 $string['generalnoticemode'] = 'Modo de Aviso General';
-$string['generalnoticemodedesc'] = 'Elija cómo mostrar los avisos generales';
-$string['generalnoticemode_off'] = 'Desactivado';
-$string['generalnoticemode_info'] = 'Información';
-$string['generalnoticemode_danger'] = 'Alerta Crítica';
+$string['generalnoticemodedesc'] = 'Elija cómo mostrar el aviso general. Diferentes modos utilizan diferentes estilos para indicar la importancia del mensaje.';
+$string['generalnoticemode_off'] = 'Desactivado - Sin aviso';
+$string['generalnoticemode_info'] = 'Información - Aviso informativo azul';
+$string['generalnoticemode_danger'] = 'Alerta Crítica - Aviso de advertencia rojo';
 
 $string['chatheading'] = 'Configuración del Chat';
-$string['enable_chat'] = 'Habilitar Chat';
-$string['enable_chatdesc'] = 'Habilitar la funcionalidad de chat';
+$string['enable_chat'] = 'Habilitar Chat en Vivo';
+$string['enable_chatdesc'] = 'Habilite el soporte de chat en vivo para usuarios que han iniciado sesión. Esta función permite la comunicación en tiempo real entre usuarios y personal de soporte.';
 $string['tawkto_embed_url'] = 'URL de Tawk.to';
-$string['tawkto_embed_urldesc'] = 'Ingrese su URL del widget de Tawk.to';
+$string['tawkto_embed_urldesc'] = 'Ingrese su URL completa del widget de Tawk.to. Puede obtenerla desde su panel de control de Tawk.to después de crear una cuenta y configurar su widget de chat.';
 
 $string['accessibilityheading'] = 'Configuración de Accesibilidad';
 $string['accessibility_widget'] = 'Widget de Accesibilidad';
-$string['accessibility_widgetdesc'] = 'Habilitar o deshabilitar el widget de accesibilidad';
+$string['accessibility_widgetdesc'] = 'Habilite un widget de accesibilidad que permita a los usuarios personalizar su experiencia visual con funciones como ajuste de tamaño de texto, modos de contraste y otras opciones de accesibilidad.';
 
 $string['contentprotectionheading'] = 'Configuración de Protección de Contenido';
 $string['copypaste_prevention'] = 'Prevenir Copiar/Pegar';
-$string['copypaste_preventiondesc'] = 'Habilitar la prevención de copiar y pegar para roles seleccionados';
+$string['copypaste_preventiondesc'] = 'Habilite funciones de prevención de copiar y pegar para proteger el contenido del curso de copias no autorizadas. Se puede aplicar a roles de usuario específicos.';
 $string['copypaste_roles'] = 'Roles Protegidos';
-$string['copypaste_rolesdesc'] = 'Seleccione qué roles serán afectados por la prevención de copiar/pegar';
+$string['copypaste_rolesdesc'] = 'Seleccione qué roles de usuario tendrán restricciones de copiar/pegar. Solo los usuarios con estos roles se verán afectados por las restricciones.';
 
 $string['loginsliderheading'] = 'Configuración del Slider de Inicio de Sesión';
 $string['slideheading'] = 'Diapositiva {$a}';
@@ -147,24 +162,17 @@ $string['footersettings'] = 'Configuración del Pie de Página';
 $string['footeraboutheading'] = 'Sección Acerca de del Pie de Página';
 
 $string['loginsettings'] = 'Configuración de Inicio de Sesión';
-$string['loginimage'] = 'Imagen de Fondo de Inicio de Sesión';
-$string['loginimagedesc'] = 'Subir una imagen para el fondo de la página de inicio de sesión.';
-$string['loginbg_color'] = 'Color de Fondo de Inicio de Sesión';
-$string['loginbg_colordesc'] = 'Seleccione el color de fondo para la página de inicio de sesión.';
 
 // Mantener el texto predeterminado de "acerca de"
 $string['abouttext_default'] = '<p style="text-align: center;"></p>
     <p style="text-align: left;">
     </p>
-    <p style="text-align: center;"><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><b>IngeWeb - Soluciones para triunfar en Internet<br></b></span><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">Expertos en Moodle, BigBlueButton, WordPress y Joomla.<br></span><strong style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">www.ingeweb.co</strong></p>
+    <p style="text-align: center;"><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><b>Fondo Nacional de Garantías<br></b></span><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">Facilitando el acceso al crédito para las pequeñas empresas en Colombia<br></span><strong style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">www.fng.gov.co</strong></p>
     <p></p>';
 
 $string['fnghelp'] = "Ayuda de la Plataforma";
-$string['fngfeedback'] = "Feedback de fng";
-$string['fngsupport'] = "Soporte fng";
+$string['fngfeedback'] = "Feedback del FNG";
+$string['fngsupport'] = "Soporte FNG";
 
 $string['enrolleduserscountvisibilityhead'] = "Mostrar información de 'Estudiantes matriculados'";
 $string['enrolleduserscountvisibilitydesc'] = "Deshabilitar para ocultar la información de 'Estudiantes matriculados'";
-
-$string['police_autorization']='Autorización para el tratamiento de datos en Medios de Recolección';
-$string['police_autorization_help']='Al diligenciar este formulario autorizo a CAJA SANTANDEREANA DE SUBSIDIO FAMILIAR "fng", ubicada en CARRERA 27 # 61 – 78 PUERTA DEL SOL y con teléfono de contacto 6434444, para que recolecte, almacene, use, circule y/o suprima mis datos personales con la finalidad de realizar el registro para realización de cursos virtuales y las demás  finalidades incorporadas en la Política de Tratamiento de Información disponible en www.fng.com, la cual declaro conocer. Así mismo, conozco que como titular me asisten los derechos a conocer, actualizar, rectificar y suprimir mis datos y revocar la autorización.';
